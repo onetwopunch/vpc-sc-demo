102a31df40bdfd0293b333c0848d9a5bc9215985
Ryan Canty <jrcanty@gmail.com>
2019-11-15 13:48:18 -0800

    Use CFT VPC SC module instead

--- org.tf
+++ org.tf
@@ -17,42 +17,32 @@ resource "google_organization_policy" "external_ip_policy" {
 ## VPC Service Controls
 ###########################
 
-locals {
-  access_policy = "accessPolicies/${google_access_context_manager_access_policy.access_policy.name}"
-  access_level_name = "${local.access_policy}/accessLevels/terraform_allowed"
-}
-
-resource "google_access_context_manager_access_policy" "access_policy" {
-  provider = "google"
-  parent   = "organizations/${var.org_id}"
-  title    = "Test Policy"
-}
-
-resource "google_access_context_manager_access_level" "terraform_access" {
-  parent      = local.access_policy
-  name        = local.access_level_name 
-  title       = "chromeos_no_lock"
-  basic {
-    conditions {
-      members = ["serviceAccount:${var.terraform_service_account}"]
-    }
-  }
-}
-
 data "google_project" "project1" {
   project_id = var.project1_id
 }
-
-resource "google_access_context_manager_service_perimeter" "regular_service_perimeter" {
-  provider       = "google"
-  parent         = local.access_policy
-  perimeter_type = "PERIMETER_TYPE_REGULAR"
-  name           = "${local.access_policy}/servicePerimeters/${var.perimeter_name}"
-  title          = "${var.perimeter_name}"
-
-  status {
-    restricted_services = ["bigquery.googleapis.com", "storage.googleapis.com"]
-    resources           = ["projects/${data.google_project.project1.number}"]
-    access_levels       = [local.access_level_name]
-  }
+module "org_policy" {
+  source      = "terraform-google-modules/vpc-service-controls/google"
+  version     = "~> 1.0.1"
+  parent_id   = var.org_id
+  policy_name = "VPC SC Demo Policy"
 }
+
+module "access_level_members" {
+  source  = "terraform-google-modules/vpc-service-controls/google//modules/access_level"
+  version = "~> 1.0.1"
+  policy  = module.org_policy.policy_id
+  name    = "terraform_members"
+  members = ["serviceAccount:${var.terraform_service_account}"]
+}
+
+module "regular_service_perimeter_1" {
+  source              = "terraform-google-modules/vpc-service-controls/google//modules/regular_service_perimeter"
+  version             = "~> 1.0.1"
+  policy              = module.org_policy.policy_id
+  perimeter_name      = "regular_perimeter_1"
+  description         = "Perimeter shielding projects"
+  resources           = [data.google_project.project1.number]
+  access_levels       = [module.access_level_members.name]
+  restricted_services = ["bigquery.googleapis.com", "storage.googleapis.com"]
+}
+
